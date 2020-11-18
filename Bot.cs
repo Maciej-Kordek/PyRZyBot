a9a0668506cae778fa6de438adb9a3f274af49f0
Medial <jubsgeg@gmail.com>
2020-11-18 22:24:58 +0100

    init

---
+++ Bot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace PyRZyBot
+{
+    internal class Bot
+    {
+
+        ConnectionCredentials credentials = new ConnectionCredentials(TwitchInfo.BotName, TwitchInfo.BotToken);
+        TwitchClient client;
+        internal void Connect(bool isLogging)
+        {
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(5)
+            };
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+            client = new TwitchClient(customClient);
+            client.Initialize(credentials, TwitchInfo.ChannelName);
+
+            if (isLogging)
+                client.OnLog += Client_OnLog;
+
+            client.OnMessageReceived += Client_OnMessageReceived;
+
+            client.Connect();
+        }
+
+        private void Client_OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            if (e.ChatMessage.Username == "nightbot") { return; }
+
+            if (e.ChatMessage.Message.Contains("xD") || e.ChatMessage.Message.Contains("XD"))
+            {
+                client.SendMessage(TwitchInfo.ChannelName, "xD");
+            }
+
+        }
+
+        private void Client_OnLog(object sender, OnLogArgs e)
+        {
+            Console.WriteLine(e.Data);
+        }
+
+        internal void Disconnect()
+        {
+            client.Disconnect();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PyRZyBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot MedBot = new Bot();
+
+            MedBot.Connect(true);
+
+            Console.ReadLine();
+
+            MedBot.Disconnect();
+        }
+    }
+}
